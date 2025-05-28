fb2eedea2331b0850d1f0cfc45e1defd445da047
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-28 12:48:16 +0530

    spring security

---
+++ security/src/main/java/com/example/security/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package com.example.security.config;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("application/json");
+        String jsonResponse = "{\"success\": false, \"message\": \"Access Denied\", \"data\": null}";
+        response.getWriter().write(jsonResponse);
+    }
+}
