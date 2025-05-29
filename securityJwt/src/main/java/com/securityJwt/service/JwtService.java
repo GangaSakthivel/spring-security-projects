a38ef4c4d305b825b5324bb857a628079cbe7e63
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-29 12:10:08 +0530

    spring security jwt authentication and authorization

--- securityJwt/src/main/java/com/securityJwt/service/JwtService.java
+++ securityJwt/src/main/java/com/securityJwt/service/JwtService.java
@@ -1,11 +1,43 @@
 package com.securityJwt.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.websocket.Decoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private static final String secretKey = "3qNE3R6K9dmQZNj+lUc9aTdRcIFJTT7LRljuNcIAnje3jofbUPkMQFHWYuy3kLPO";
+
     public String extractUserName(String token) {
-        return
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+
+
     }
 }
