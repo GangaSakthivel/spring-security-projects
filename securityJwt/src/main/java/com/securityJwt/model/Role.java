664301087e768579c43e5e09bf89f72879ef6072
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-29 10:53:24 +0530

    spring security jwt authentication and authorization

---
+++ securityJwt/src/main/java/com/securityJwt/model/Role.java
@@ -0,0 +1,6 @@
+package com.securityJwt.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- securityJwt/src/main/java/com/securityJwt/model/User.java
+++ securityJwt/src/main/java/com/securityJwt/model/User.java
@@ -6,6 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,10 +19,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue //default value is AUTO
     private Integer id;
     private String firstName;
     private String lastName;
@@ -24,4 +30,43 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return List.of(new SimpleGrantedAuthority(role.name())); //ROLE_USER, ROLE_ADMIN
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
