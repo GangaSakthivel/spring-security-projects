d37a6bdb681e18cb4a2536b1b0252c6e71751085
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-29 11:37:23 +0530

    spring security jwt authentication and authorization

---
+++ securityJwt/src/main/java/com/securityJwt/config/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.securityJwt.config;
+
+
+import com.securityJwt.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor //generates constructor for all final field (also fields annotated with @Nonnull in a class)
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if(authHeader != null || authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);//extract the username from jwt
+
+
+    }
+}
